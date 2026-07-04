e3f7bbe4aaeb1d0c35823011c8bfd9fa339daf85
E7aman <elaman10230717@gmail.com>
2026-07-05 02:24:04 +0500

    Add change password action button for clients

--- barbershop/barbers/admin.py
+++ barbershop/barbers/admin.py
@@ -1,18 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.shortcuts import redirect
+from django.urls import reverse
 from unfold.admin import ModelAdmin as UnfoldModelAdmin
+from unfold.decorators import action # <--- Не забудь импортировать декоратор
 from .models import User, Service, MasterProfile, WorkSchedule, MasterOffDay, Appointment
 
-# Используем стандартный UserAdmin от Django, но добавляем обертку Unfold через миксин
 @admin.register(User)
-class CustomUserAdmin(BaseUserAdmin, UnfoldModelAdmin):  # <--- Магия множественного наследования
+class CustomUserAdmin(BaseUserAdmin, UnfoldModelAdmin):
     list_display = ('username', 'first_name', 'phone', 'role', 'is_staff')
     list_filter = ('role', 'is_staff')
     
-    # Добавляем кастомные поля в стандартные филдсеты Django
+    # Выводим большую кнопку "Сменить пароль" в правый верхний угол страницы редактирования
+    actions_detail = ["change_password_action"]
+
     fieldsets = BaseUserAdmin.fieldsets + (
         ('Дополнительные поля ролей', {'fields': ('role', 'phone', 'tg_chat_id')}),
     )
+
+    @action(description="Сменить пароль", url_path="change-password")
+    def change_password_action(self, request, object_id):
+        # Перенаправляем админа/клиента на стандартную защищенную форму смены пароля Django
+        return redirect(reverse("admin:auth_user_password_change", args=[object_id]))
 
 # 2. Записи (Appointments)
 @admin.register(Appointment)
